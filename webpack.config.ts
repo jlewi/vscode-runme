16e83c98c234785b1c793d8f5e3a5f77483f1ed4
Jeremy Lewi <jeremy@lewi.us>
2024-09-02 17:46:47 -0700

    First crack at trying to build a vscode web extension for RunMe
    * Added a `browser` rule to package.json
    * Added some scripts to package.json to try to compile it using webpack.
    * Added a webpack configuration for the web.
    
    * When I compile it with webpack I get more than 400 errors. A lot of these look like they are due to
      trying to run node modules in the browser.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import { Configuration, DefinePlugin } from 'webpack'
+import { Configuration, DefinePlugin, webpack, ProvidePlugin} from 'webpack'
 
 enum Mode {
   PRODUCTION = 'production',
@@ -83,4 +83,59 @@ const extensionConfig: Configuration = {
   ]
 }
 
-export default [extensionConfig, rendererConfig]
+// TODO(jeremy): This is an attempt to create a webpack configuration
+// to compile RunMe to work in vscode for web.
+// It is based on https://github.com/aeschli/vscode-css-formatter/blob/73cda04a9d9bfd2195d899d5e6d69484b4402345/webpack.config.js#L4
+const webConfig: Configuration = {
+  ...baseConfig,
+  // Name for this configuration so we can invoke it with `webpack --config-name web`
+  name : 'web',
+  target: 'webworker', // web extensions run in a webworker context
+  entry: {
+    // TODO(jeremy): We're starting by trying to reuse the same main file as the node extension
+    // We'll see if that works or we need to change things.
+    'extension-web': path.resolve(__dirname, 'src', 'extension', 'index.ts'), // source of the web extension main file
+    //'test/suite/index-web': './src/test/suite/index-web.ts', // source of the web extension test runner
+  },
+  // N.B. This resolve section was just copied from
+  // https://github.com/aeschli/vscode-css-formatter/blob/73cda04a9d9bfd2195d899d5e6d69484b4402345/webpack.config.js#L4
+  resolve: {
+    mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+    extensions: ['.ts', '.js'], // support ts-files and js-files
+    alias: {
+      // provides alternate implementation for node module and source files
+    },
+    fallback: {
+      // Webpack 5 no longer polyfills Node.js core modules automatically.
+      // see https://webpack.js.org/configuration/resolve/#resolvefallback
+      // for the list of Node.js core module polyfills.
+      assert: require.resolve('assert'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ProvidePlugin({
+      process: 'process/browser', // provide a shim for the global `process` variable
+    }),
+  ],
+  externals: {
+    vscode: 'commonjs vscode', // ignored because it doesn't exist
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: 'nosources-source-map', // create a source map that points to the original source file
+}
+export default [extensionConfig, rendererConfig, webConfig]
